9f6ec8a87e81c32b5942db3d6413a7f19fed84b7
Tan Dat <trantandat0723@gmail.com>
2025-10-14 22:34:22 +0700

    feat(update): create provider and custom navbar desktop & moblie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
-};
+  return (
+    <div className='overflow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  )
+}
 
 export default App;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('https://fakestoreapi.com/products')
+      const data = await res.json()
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  )
 };
 
-export default ProductContext;
+export default ProductProvider;
